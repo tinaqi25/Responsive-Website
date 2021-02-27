dc90c951adc02e050039c69f3e786d65fe1ce40e
tinaqi25 <39071151+tinaqi25@users.noreply.github.com>
2021-02-27 01:00:37 -0500

    Update Stylesheet

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const burger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav_links");
+const lineOne = document.querySelector(".one");
+const lineTwo = document.querySelector(".two");
+const lineThree = document.querySelector(".three");
 
 
 const toggleNav =()=>{
     nav.classList.toggle("nav_active");
+    lineOne.classList.toggle("cross");
+    lineTwo.classList.toggle("cross");
+    lineThree.classList.toggle("cross");
 };
 
 
 burger.onclick= function(event){
     toggleNav();
+    
 };
